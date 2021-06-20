62988ebf7b78b8ba3757d102eee796ccf9bd0201
三浦政大 <masao.miura@i-vinci.co.jp>
2021-06-20 21:36:37 +0900

    add useState useRef

--- src/feature/auth/components/Auth.js
+++ src/feature/auth/components/Auth.js
@@ -1,7 +1,10 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState, useRef } from 'react';
 
 const Auth = (props) => {
   const { auth, authStart } = props;
+
+  const [title, setTitle] = useState('');
+  const bodyRef = useRef();
 
   useEffect(() => {
     authStart();
@@ -13,10 +16,34 @@ const Auth = (props) => {
     return <div>{auth.error}</div>;
   }
 
+  const sendData = (event) => {
+    event.preventDefault();
+    console.log(`title: ${title}`);
+    console.log(`body: ${bodyRef.current.value}`);
+    setTitle('');
+    bodyRef.current.value = '';
+  };
+
   return (
     <Fragment>
       <div>{auth.user.name}</div>
       <div>{auth.user.email}</div>
+      <form onSubmit={sendData}>
+        <div>
+          <label htmlFor='title'>タイトル</label>
+          <input
+            id='title'
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor='body'>本文</label>
+          <input id='body' type='text' ref={bodyRef} />
+        </div>
+        <button>送信</button>
+      </form>
     </Fragment>
   );
 };
